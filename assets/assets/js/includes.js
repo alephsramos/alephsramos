55034d0f31326a3b2ac2b016674a8a8dd0441ca7
aleph ramos <105456824+alephsramos@users.noreply.github.com>
2024-07-20 17:17:03 -0300

    organização

--- assets/assets/js/includes.js
+++ assets/assets/js/includes.js
@@ -7,8 +7,20 @@ function loadHTML(file, elementId) {
         });
 }
 
+// Função para carregar CSS e JS de includes.html
+function loadIncludes(file) {
+    fetch(file)
+        .then(response => response.text())
+        .then(data => {
+            const includesDiv = document.createElement('div');
+            includesDiv.innerHTML = data;
+            document.head.appendChild(includesDiv);
+        });
+}
+
 // Carregar componentes HTML
 document.addEventListener("DOMContentLoaded", function() {
     loadHTML('includes/header.html', 'header-placeholder');
     loadHTML('includes/footer.html', 'footer-placeholder');
+    loadIncludes('includes/includes.html');
 });
